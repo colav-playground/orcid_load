d06f1aef3272dc89a6743221d2b2240e28cbc6e4
Omar Zapata <andresete.chaos@gmail.com>
2025-03-22 17:31:48 -0500

    added script to load summaries from xml files in ORCID to MongoDB in parallel

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # orcid_load
 scripts to load ORCID in MongoDB
+
+# install dependencies with 
+pip install xmltodict joblib pymongo
+
+
+edit load.py to set the path to the ORCID data files and the MongoDB connection string
+
+run with
+``` 
+python load.py
+``` 
+
+NOTE:
+
+* checkpoint is not supported yet
+* we are only loading the summaries (authors information) not works.
+* if something fails pelase start again from the beginning
+* the index "record:record.common:orcid-identifier.common:uri" is created to speed up the queries search with orcid with full url ex: https://orcid.org/0000-0002-1825-0097
---
+++ load.py
@@ -0,0 +1,38 @@
+import glob
+import xmltodict
+from pymongo import MongoClient
+from joblib import Parallel, delayed
+
+orcid_summaries_path = "/storage/orcid/ORCID_2024_10_summaries/"
+db_name = "orcid"
+col_name = "summaries"
+jobs=72
+
+
+db = MongoClient()[db_name]
+col = db[col_name]
+
+summaries = glob.glob(orcid_summaries_path + "/*/*.xml")
+
+print("INFO: processing {} files from {}".format(len(summaries),orcid_summaries_path))
+
+def get_xml_data(file):
+    f = open(file)
+    data = f.read()
+    f.close()
+    return data
+
+
+def parse_xmltodict(file):
+    data = get_xml_data(file)
+    return xmltodict.parse(data)
+
+
+def process_one(col, file):
+    data = parse_xmltodict(file)
+    col.insert_one(data)
+
+
+Parallel(n_jobs=jobs, verbose=10, backend="threading")(
+    delayed(process_one)(col, file) for file in summaries
+)

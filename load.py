a661208a19e72d5fce1d3289bc89216c1630bd28
Omar Zapata <andresete.chaos@gmail.com>
2025-03-23 11:34:05 -0500

    added support to save locked profiles in a locked collection

--- load.py
+++ load.py
@@ -6,12 +6,13 @@ from joblib import Parallel, delayed
 orcid_summaries_path = "/storage/orcid/ORCID_2024_10_summaries/"
 db_name = "orcid"
 col_name = "summaries"
+col_name_locked = "summaries_locked"
 jobs=72
 
 
 db = MongoClient()[db_name]
 col = db[col_name]
-
+col_locked = db[col_name_locked]
 summaries = glob.glob(orcid_summaries_path + "/*/*.xml")
 
 print("INFO: processing {} files from {}".format(len(summaries),orcid_summaries_path))
@@ -30,8 +31,12 @@ def parse_xmltodict(file):
 
 def process_one(col, file):
     data = parse_xmltodict(file)
-    del data['record:record']['activities:activities-summary'] # produces "BSON document too large" in MongoDB
-    col.insert_one(data)
+    if 'record:record' in data:
+        del data['record:record']['activities:activities-summary'] # produces "BSON document too large" in MongoDB
+        col.insert_one(data)
+    else:
+        print("WARNING: 'record:record' not found, probably locked")
+        col_locked.insert_one(data)
 
 
 Parallel(n_jobs=jobs, verbose=10, backend="threading")(
